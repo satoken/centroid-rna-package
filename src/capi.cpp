e9744658505ee5efcba9b12b5497697f0e3270fc
sato-kengo <sato-kengo@localhost>
2009-02-05 06:21:15 +0000

    * capi.cpp: support for deamon.

--- src/capi.cpp
+++ src/capi.cpp
@@ -8,30 +8,48 @@
 #include <new>
 #include <stdexcept>
 #include "capi.h"
-#include "centroid.h"
-#include "bp.h"
+#include "centroid_fold.h"
 
-typedef SCFG::BP::Table<double> BPTable;
+class CFWrapper
+{
+public:
+  CFWrapper()
+  {
+    impl_ = new CentroidFold(CentroidFold::CONTRAFOLD);
+  }
 
-static BPTable bp;
+  ~CFWrapper()
+  {
+    delete impl_;
+  }
+
+  int
+  centroid_fold(const char* seq, char* str, float g)
+  {
+    try {
+      impl_->calculate_posterior(seq);
+      unsigned int l=strlen(seq);
+      std::string paren(l, '.');
+      impl_->decode_structure(g, paren);
+      strcpy(str, paren.c_str());
+    } catch (std::bad_alloc& e) {
+      return CENTROID_BAD_ALLOC;
+    } catch (std::length_error& e) {
+      return CENTROID_LENGTH_ERR;
+    } catch (std::exception& e) {
+      return CENTROID_UNKNOWN_ERR;
+    }
+    return CENTROID_SUCCESS;
+  }
+
+private:
+  CentroidFold* impl_;
+};
+
+static CFWrapper cf;
 
 int
 centroidfold(const char *seq, char *str, float g)
 {
-  try {
-    if (bp.reserved_size()<RESERVED_LENGTH)
-      bp.reserve(RESERVED_LENGTH);
-    bp.contra_fold(seq);
-    unsigned int l=strlen(seq);
-    std::string paren(l, '.');
-    SCFG::Centroid::execute(bp, paren, g);
-    strcpy(str, paren.c_str());
-  } catch (std::bad_alloc& e) {
-    return CENTROID_BAD_ALLOC;
-  } catch (std::length_error& e) {
-    return CENTROID_LENGTH_ERR;
-  } catch (std::exception& e) {
-    return CENTROID_UNKNOWN_ERR;
-  }
-  return CENTROID_SUCCESS;
+  return cf.centroid_fold(seq, str, g);
 }
