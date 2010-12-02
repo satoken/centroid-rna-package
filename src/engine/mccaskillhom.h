89ca3fec54793b665b1f568f57b34ec5485e258c
m-hamada <m-hamada@localhost>
2010-12-02 04:42:39 +0000

    Added several files in engine directory for CentroidHomfold

---
+++ src/engine/mccaskillhom.h
@@ -0,0 +1,32 @@
+// Copyright 2009, 2010 Michiaki Hamada
+
+#ifndef __INC_ENGINE_MCCASKILLHOM_H__
+#define __INC_ENGINE_MCCASKILLHOM_H__
+
+#include "../centroid_fold.h"
+
+#ifdef HAVE_LIBRNA
+class McCaskillHomModel : public CentroidFold<TH>
+{
+public:
+  McCaskillHomModel(const std::string& engine_a, bool canonical_only, uint max_bp_dist,
+		    const char* param=NULL, uint seed=0, bool run_as_mea=false);
+  virtual ~McCaskillHomModel() { }
+
+  // interface implementations
+  virtual void set_constraint(const std::string& str);
+  virtual void calculate_posterior(const TH& seq);
+/*   virtual void prepare_stochastic_traceback(const std::string& seq); */
+/*   virtual std::vector<int> stochastic_traceback(const std::string& seq); */
+/*   virtual void clean_stochastic_traceback(const std::string& seq); */
+
+private:
+  bool canonical_only_;
+  int bk_st_back_;
+  std::string str_;
+  const char* param_;
+  std::string engine_a_;
+};
+#endif
+
+#endif  //  __INC_ENGINE_MCCASKILLHOM_H__
